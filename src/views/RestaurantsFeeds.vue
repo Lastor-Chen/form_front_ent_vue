98502dcaf42fdbaa6988cfab36de24aaa283ab8e
Lastor <lastoraya@gmail.com>
2020-02-23 12:52:02 +0800

    [加入] 餐廳Feeds頁，最新餐廳組件

--- src/views/RestaurantsFeeds.vue
+++ src/views/RestaurantsFeeds.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="container py-5">
     <NavTabs/>
+
     <h1 class="mt-5">最新動態</h1>
+    <hr>
+    <div class="row">
+      <div class="col-md-6">
+        <h3>最新餐廳</h3>
+        <NewestRestaurants/>
+      </div>
+      <div class="col-md-6">
+        <h3>最新評論</h3>
+      </div>
+    </div>
   </div>
 </template>
 
