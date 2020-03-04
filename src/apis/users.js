d315fb0ac6cb4793da6fb1006e7f9cee155df0e4
Lastor <lastoraya@gmail.com>
2020-03-04 16:54:45 +0800

    [修改] 實裝 UsersTop 頁， follow 功能串接 API

--- src/apis/users.js
+++ src/apis/users.js
@@ -4,25 +4,30 @@ function getToken() {
   return localStorage.getItem('token')
 }
 
+const config = {
+  headers: { Authorization: `Bearer ${getToken()}` }
+}
+
 export default {
   addFavorite(restaurantId) {
-    return apiHelper.post(`/favorite/${restaurantId}`, null, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.post(`/favorite/${restaurantId}`, null, config)
   },
   deleteFavorite(restaurantId) {
-    return apiHelper.delete(`/favorite/${restaurantId}`, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.delete(`/favorite/${restaurantId}`, config)
   },
   addLike(restaurantId) {
-    return apiHelper.post(`/like/${restaurantId}`, null, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.post(`/like/${restaurantId}`, null, config)
   },
   deleteLike(restaurantId) {
-    return apiHelper.delete(`/like/${restaurantId}`, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    }) 
+    return apiHelper.delete(`/like/${restaurantId}`, config) 
+  },
+  getTopUsers() {
+    return apiHelper.get(`/users/top`, config)
+  },
+  addFollowing(userId) {
+    return apiHelper.post(`/following/${userId}`, null, config)
+  },
+  deleteFollowing(userId) {
+    return apiHelper.delete(`/following/${userId}`, config)
   }
 }
---
+++ src/components/UserTopCard.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="col-3">
+    <router-link :to="`/users/${user.id}`">
+      <img
+        :src="user.image || 'http://via.placeholder.com/300x300?text=No+Image'"
+        width="140px"
+        height="140px"
+      >
+    </router-link>
+    <h2>{{user.name}}</h2>
+    <span class="badge badge-secondary">追蹤人數：{{user.FollowerCount}}</span>
+    <p class="mt-3">
+      <button
+        v-if="user.isFollowed"
+        type="button"
+        class="btn btn-danger"
+        @click="deleteFollowing(user.id)"
+      >
+        取消追蹤
+      </button>
+      <button
+        v-else
+        type="button"
+        class="btn btn-primary"
+        @click="addFollowing(user.id)"
+      >
+        追蹤
+      </button>
+    </p>
+  </div>
+</template>
+
+<script>
+import usersAPI from '../apis/users.js'
+import { Toast } from '../utils/helpers.js'
+
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      isLoading: false
+    }
+  },
+  methods: {
+    toggleFollow(action) {
+      this.user.isFollowed = !this.user.isFollowed
+
+      if (action === 'add') { this.user.FollowerCount += 1 }
+      if (action === 'delete') { this.user.FollowerCount -= 1 }
+      
+      this.$parent.users.sort((a, b) => b.FollowerCount - a.FollowerCount)
+    },
+    async addFollowing(userId) {
+      try {
+        if (this.isLoading) return false
+        this.isLoading = true
+
+        this.toggleFollow('add')
+
+        const { data } = await usersAPI.addFollowing(userId)
+        if (data.status !== 'success') throw 'serveError'
+        this.isLoading = false
+      
+      } catch(err) {
+        this.isLoading = false
+        Toast.fire({
+          icon: 'error',
+          title: '無法加入追蹤，請稍後再試'
+        })
+      }
+    },
+    async deleteFollowing(userId) {
+      try {
+        if (this.isLoading) return false
+        this.isLoading = true
+
+        this.toggleFollow('delete')
+
+        const { data } = await usersAPI.deleteFollowing(userId)
+        if (data.status !== 'success') throw 'serveError'
+        this.isLoading = false
+
+      } catch(err) {
+        this.isLoading = false
+        Toast.fire({
+          icon: 'error',
+          title: '無法取消追蹤，請稍後再試'
+        })
+      }
+    }
+  }
+}
+</script>
