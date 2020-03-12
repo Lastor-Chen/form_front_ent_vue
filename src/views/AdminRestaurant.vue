0e097000397aa9d6fac416e73637fc6f42c1c2a9
Lastor <lastoraya@gmail.com>
2020-03-12 23:26:43 +0800

    [修改] 實裝 admin 餐廳詳細，API 串接

--- src/views/AdminRestaurant.vue
+++ src/views/AdminRestaurant.vue
@@ -39,28 +39,8 @@
 
 <script>
 import { emptyImageFilter } from '../utils/mixins.js'
-
-const dummyData = {
-  restaurant: {
-    id: 2,
-    name: 'Mrs. Mckenzie Johnston',
-    tel: '567-714-6131 x621',
-    address: '61371 Rosalinda Knoll',
-    opening_hours: '08:00',
-    description:
-      'Quia pariatur perferendis architecto tenetur omnis pariatur tempore.',
-    image: 'https://loremflickr.com/320/240/food,dessert,restaurant/?random=2',
-    createdAt: '2019-06-22T09:00:43.000Z',
-    updatedAt: '2019-06-22T09:00:43.000Z',
-    CategoryId: 3,
-    Category: {
-      id: 3,
-      name: '義大利料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z'
-    }
-  }
-}
+import adminAPI from '../apis/admin.js'
+import { Toast } from '../utils/helpers.js'
 
 export default {
   name: 'AdminRestaurant',
@@ -79,24 +59,39 @@ export default {
       }
     }
   },
-  mounted() {
+  created() {
     const restaurantId = this.$route.params.id
     this.fetchRestaurant(restaurantId)
   },
+  beforeRouteUpdate(to, from , next) {
+    this.fetchRestaurant(to.params.id)
+    next()
+  },
   methods: {
-    fetchRestaurant(restaurantId) {
-      const { restaurant } = dummyData
-      this.restaurant = {
-        ...this.restaurant,
-        id: restaurant.id,
-        name: restaurant.name,
-        categoryName: restaurant.Category.name,
-        image: restaurant.image,
-        openingHours: restaurant.opening_hours,
-        tel: restaurant.tel,
-        address: restaurant.address,
-        description: restaurant.description
+    async fetchRestaurant(restaurantId) {
+      try {
+        const { data, statusText } = await adminAPI.restaurants.getOne(restaurantId)
+        if (statusText !== 'OK') throw 'serverError'
+
+        const { restaurant } = data
+        this.restaurant = {
+          id: restaurant.id,
+          name: restaurant.name,
+          categoryName: restaurant.Category.name,
+          image: restaurant.image,
+          openingHours: restaurant.opening_hours,
+          tel: restaurant.tel,
+          address: restaurant.address,
+          description: restaurant.description
+        }
+
+      } catch(err) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得餐廳資料，請稍後再試'
+        })
       }
+      
     }
   }
 }
