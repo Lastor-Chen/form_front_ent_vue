ba3c3aef66e2f3f0656d8f114000e175cfba101d
Lastor-Chen <lastoraya@gmail.com>
2020-04-04 21:36:43 +0800

    [實驗] 引入 vue-session 套件

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueSession from 'vue-session'
 import './assets/application.scss'
 import { emptyImageFilter } from './utils/mixins.js'
 
 Vue.config.productionTip = false
 Vue.mixin(emptyImageFilter)
+Vue.use(VueSession)
 
 new Vue({
   router,
